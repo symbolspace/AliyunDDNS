a309b0df43d53407cfed2f15b8837a87d09a3359
symbolspace <symbolspace@outlook.com>
2020-02-03 23:14:23 +0800

    docker project imp

---
+++ src/AliyunDDNS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AliyunDDNS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var log = new ConsoleLog("AliyunDDNS")) {
+                log.Info($"aliyun ddns v{typeof(Program).Assembly.GetName().Version} by symbolspace@outlook.com");
+                log.Info("");
+
+                // create ddns instance
+                //   default config : ~/aliyun.ddns.config.json
+                var ddns = new module.AliyunDDNS.DomainIPSyncService();
+                // set log 
+                ddns.Log = log;
+                // start task
+                ddns.Start();
+
+                System.Console.ReadLine();
+                ddns.Stop();
+            }
+        }
+    }
+}
