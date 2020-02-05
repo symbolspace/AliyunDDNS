99099ac582fb60cd3316a4c0a0409e5c301810eb
symbolspace <symbolspace@outlook.com>
2020-02-05 10:59:19 +0800

    run at host

--- src/AliyunDDNS/Program.cs
+++ src/AliyunDDNS/Program.cs
@@ -1,26 +1,31 @@
-﻿using System;
+﻿//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AliyunDDNS {
     class Program {
-        static void Main(string[] args) {
-            var log = new ConsoleLog("AliyunDDNS");
-            log.Info($"aliyun ddns v{typeof(module.AliyunDDNS.DomainIPSyncService).Assembly.GetName().Version} by symbolspace@outlook.com");
-
-            // create ddns instance
-            //   default config : ~/aliyun.ddns.config.json
-            string configFile = "/app/aliyun.ddns.config.json";
-            if (!System.IO.File.Exists(configFile))
-                configFile = "~/aliyun.ddns.config.json";
-            log.Info($"   config:{configFile}");
-
-            log.Info("    create ddns instance ...");
-            var ddns = new module.AliyunDDNS.DomainIPSyncService(configFile);
-            // set log 
-            ddns.Log = log;
-            // start task
-            ddns.Start();
-            log.Info("    console readkey ...");
-            Console.Read();
+        public static void Main(string[] args) {
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            return Host.CreateDefaultBuilder(args)
+                 .ConfigureServices((serviceProvider) => {
+                     var log = new ConsoleLog("AliyunDDNS");
+                     log.Info($"aliyun ddns v{typeof(module.AliyunDDNS.DomainIPSyncService).Assembly.GetName().Version} by symbolspace@outlook.com");
+                     serviceProvider.AddSingleton<ILog>(log);
+
+                     string configFile = "/app/aliyun.ddns.config.json";
+                     if (!System.IO.File.Exists(configFile))
+                         configFile = "~/aliyun.ddns.config.json";
+                     log.Info($"   config:{configFile}");
+                     var ddns = new module.AliyunDDNS.DomainIPSyncService(configFile);
+                     ddns.Log = log;
+                     serviceProvider.AddSingleton(ddns);
+                     ddns.Start();
+                 });
+        }
+
     }
 }
