3195a6f92448000fe6029c62a8568d60223595fc
symbolspace <symbolspace@outlook.com>
2020-02-05 11:56:42 +0800

    no log

--- src/AliyunDDNS/Program.cs
+++ src/AliyunDDNS/Program.cs
@@ -12,16 +12,16 @@ namespace AliyunDDNS {
         public static IHostBuilder CreateHostBuilder(string[] args) {
             return Host.CreateDefaultBuilder(args)
                  .ConfigureServices((serviceProvider) => {
-                     var log = new ConsoleLog("AliyunDDNS");
-                     log.Info($"aliyun ddns v{typeof(module.AliyunDDNS.DomainIPSyncService).Assembly.GetName().Version} by symbolspace@outlook.com");
-                     serviceProvider.AddSingleton<ILog>(log);
+                     //var log = new ConsoleLog("AliyunDDNS");
+                     //log.Info($"aliyun ddns v{typeof(module.AliyunDDNS.DomainIPSyncService).Assembly.GetName().Version} by symbolspace@outlook.com");
+                     //serviceProvider.AddSingleton<ILog>(log);
 
                      string configFile = "/app/aliyun.ddns.config.json";
                      if (!System.IO.File.Exists(configFile))
                          configFile = "~/aliyun.ddns.config.json";
-                     log.Info($"   config:{configFile}");
+                     //log.Info($"   config:{configFile}");
                      var ddns = new module.AliyunDDNS.DomainIPSyncService(configFile);
-                     ddns.Log = log;
+                     //ddns.Log = log;
                      serviceProvider.AddSingleton(ddns);
                      ddns.Start();
                  });
